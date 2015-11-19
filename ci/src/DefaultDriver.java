d7fa8238a68ce048584632303cfdd568591f05f9
George <geomois@gmail.com>
2015-11-19 11:31:03 +0100

    Init Example 2

---
+++ ci/src/DefaultDriver.java
@@ -0,0 +1,86 @@
+import cicontest.algorithm.abstracts.AbstractDriver;
+import cicontest.algorithm.abstracts.DriversUtils;
+import cicontest.torcs.client.Action;
+import cicontest.torcs.client.SensorModel;
+import cicontest.torcs.genome.IGenome;
+import cicontest.torcs.controller.extras.ABS;
+import cicontest.torcs.controller.extras.AutomatedClutch;
+import cicontest.torcs.controller.extras.AutomatedGearbox;
+import cicontest.torcs.controller.extras.AutomatedRecovering;
+
+public class DefaultDriver extends AbstractDriver {
+
+    NeuralNetwork neuralNetwork = new NeuralNetwork();
+
+    DefaultDriver() {
+        initialize();
+       // neuralNetwork = neuralNetwork.loadGenome();
+    }
+
+    public void loadGenome(IGenome genome) { }
+
+    public void initialize(){
+       this.enableExtras(new AutomatedClutch());
+       this.enableExtras(new AutomatedGearbox());
+       this.enableExtras(new AutomatedRecovering());
+       this.enableExtras(new ABS());
+    }
+
+    @Override
+    public void control(Action action, SensorModel sensors) {
+        // Example of a bot that drives pretty well; you can use this to generate data
+        action.steering = DriversUtils.alignToTrackAxis(sensors, 0.5);
+        if(sensors.getSpeed() > 60.0D) {
+            action.accelerate = 0.0D;
+            action.brake = 0.0D;
+        }
+
+        if(sensors.getSpeed() > 70.0D) {
+            action.accelerate = 0.0D;
+            action.brake = -1.0D;
+        }
+
+        if(sensors.getSpeed() <= 60.0D) {
+            action.accelerate = (80.0D - sensors.getSpeed()) / 80.0D;
+            action.brake = 0.0D;
+        }
+
+        if(sensors.getSpeed() < 30.0D) {
+            action.accelerate = 1.0D;
+            action.brake = 0.0D;
+        }
+        System.out.println(action.steering +"steering");
+        System.out.println(action.accelerate + "acceleration");
+        System.out.println(action.brake + "brake");
+
+        System.out.println(sensors.getSpeed() +"speedIn");
+        System.out.println(sensors.getAngleToTrackAxis() + "AngleToTrackAxis");
+        System.out.println(sensors.getTrackEdgeSensors()[0] + "TrackEdgeSensors");
+
+    }
+
+    public String getDriverName() {
+        return "simple example 2";
+    }
+
+    public void controlQualification(Action action, SensorModel sensors) { }
+
+    public void controlRace(Action action, SensorModel sensors) {}
+
+    public void defaultControl(Action action, SensorModel sensors){}
+
+    @Override
+    public double getSteering(SensorModel sensorModel) {
+        return 0;
+    }
+
+    @Override
+    public double getAcceleration(SensorModel sensorModel) {
+        return 0;
+    }
+
+    public double getBraking(SensorModel sensorModel){
+        return 0;
+    }
+
+}
---
+++ ci/src/DefaultDriverGenome.java
@@ -0,0 +1,12 @@
+
+import cicontest.torcs.genome.IGenome;
+import java.io.File;
+import cicontest.algorithm.abstracts.AbstractAlgorithm;
+import cicontest.algorithm.abstracts.AbstractRace;
+import cicontest.algorithm.abstracts.DriversUtils;
+import cicontest.torcs.controller.Driver;
+
+public class DefaultDriverGenome implements IGenome {
+    private static final long serialVersionUID = 6534186543165341653L;
+}
+
