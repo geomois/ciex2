45b5edf075e7f6ba616d89c8f13a14b72587e25a
George <geomois@gmail.com>
2015-12-03 19:21:18 +0100

    only for training 2nd

--- ci/src/ci/DefaultDriver.java
+++ ci/src/ci/DefaultDriver.java
@@ -1,4 +1,5 @@
 package ci;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -17,107 +18,125 @@ import cicontest.torcs.controller.extras.AutomatedRecovering;
 
 public class DefaultDriver extends AbstractDriver {
 
-//    private NeuralNetwork neuralNetwork = new NeuralNetwork();
-    private ArrayList<Double> input;
-    private ArrayList<ArrayList<Double>> inputAr;
-    private ArrayList<Double> speedAr;
-    private ArrayList<Double> steeringAr;
+	// private NeuralNetwork neuralNetwork = new NeuralNetwork();
+	private ArrayList<Double> input;
+	private ArrayList<ArrayList<Double>> inputAr;
+	private ArrayList<Double> speedAr;
+	private ArrayList<Double> steeringAr;
 	private DefaultDriverGenome driverGenome;
-    DefaultDriver() {
-        initialize();
-    }
+	private double prevSteering;
 
-    public void loadGenome(IGenome genome) {
-    	driverGenome=(DefaultDriverGenome) genome;
-    }
+	DefaultDriver() {
+		initialize();
+	}
 
-    public void initialize(){
-       this.enableExtras(new AutomatedClutch());
-       this.enableExtras(new AutomatedGearbox());
-       this.enableExtras(new AutomatedRecovering());
-       this.enableExtras(new ABS());
-       input=new ArrayList<Double>();
-       inputAr=new ArrayList<ArrayList<Double>>();
-       speedAr=new ArrayList<Double>();
-       steeringAr=new ArrayList<Double>();
-    }
+	public void loadGenome(IGenome genome) {
+		driverGenome = (DefaultDriverGenome) genome;
+	}
 
-    @Override
-    public void control(Action action, SensorModel sensors) {
-       
-    	Double [] NNOutput=new Double[2];
-    	input.clear();
-//        input.add(sensors.getTrackEdgeSensors()[8]);
-//        input.add(sensors.getTrackEdgeSensors()[10]);
-//        input.add(sensors.getTrackEdgeSensors()[9]);
-    	for(int i=0;i<sensors.getTrackEdgeSensors().length;i++){
-    		input.add(sensors.getTrackEdgeSensors()[i]);
-    	}
+	public void initialize() {
+		this.enableExtras(new AutomatedClutch());
+		this.enableExtras(new AutomatedGearbox());
+		this.enableExtras(new AutomatedRecovering());
+		this.enableExtras(new ABS());
+		input = new ArrayList<Double>();
+		inputAr = new ArrayList<ArrayList<Double>>();
+		speedAr = new ArrayList<Double>();
+		steeringAr = new ArrayList<Double>();
+		prevSteering = 0.0;
+	}
 
-    	NNOutput=driverGenome.getNNValue(input);
-    	Double desiredSpeed=NNOutput[0];
-//    	System.out.println(desiredSpeed.toString());
-    	Double bias;
-    	Double dif=input.get(0).doubleValue()-input.get(input.size()-1).doubleValue();
-    	if(dif>0.7 && dif<2.0){
-    		bias=-0.2;
-    	}else if(dif<-0.7 && dif>-2.0){
-    		bias=0.2;
-    	}else if(dif<-3.0){
-    		bias=0.5;
-    	}else if(dif>3.0){
-    		bias=-0.5;
-    	}else{
-    		bias=0.0;
-    	}
-    	
-    	action.steering=DriversUtils.alignToTrackAxis(sensors,0.5D)+bias;
-    	System.out.println(bias.toString()+" "+dif+" "+Double.toString(action.steering));
-    	if(sensors.getSpeed() > desiredSpeed) {
-            action.accelerate = 0.0D;
-            action.brake = 0.0D;
-        }
-        if(sensors.getSpeed() > desiredSpeed+10.0D) {
-            action.accelerate = 0.0D;
-            action.brake = 0.6D;
-        }
-        if(sensors.getSpeed() <= desiredSpeed) {
-            action.accelerate = (desiredSpeed - sensors.getSpeed()/4) / desiredSpeed;
-            action.brake = 0.0D;
-        }
-        if(sensors.getSpeed() < (desiredSpeed*3/4)) {
-            action.accelerate = 1.0D;
-            action.brake = 0.0D;
-        }
-        
-    	inputAr.add(input);
-    	speedAr.add(desiredSpeed);
-    	steeringAr.add(action.steering);
-    }
+	@Override
+	public void control(Action action, SensorModel sensors) {
+
+		Double[] NNOutput = new Double[2];
+		input.clear();
+		// input.add(sensors.getTrackEdgeSensors()[8]);
+		// input.add(sensors.getTrackEdgeSensors()[10]);
+		// input.add(sensors.getTrackEdgeSensors()[9]);
+		for (int i = 0; i < sensors.getTrackEdgeSensors().length; i++) {
+			input.add(sensors.getTrackEdgeSensors()[i]);
+		}
+
+		NNOutput = driverGenome.getNNValue(input);
+		Double desiredSpeed = NNOutput[0];
+		// System.out.println(desiredSpeed.toString());
+		Double bias;
+		Double dif = input.get(0).doubleValue() - input.get(input.size() - 1).doubleValue();
+		if (dif > 0.7 && dif < 2.0) {
+			bias = -0.08;
+		} else if (dif < -0.7 && dif > -2.0) {
+			bias = 0.08;
+		} else if (dif < -3.0) {
+			bias = 0.1;
+		} else if (dif > 3.0) {
+			bias = -0.1;
+		} else {
+			bias = 0.0;
+		}
+		Double currentSteer = DriversUtils.alignToTrackAxis(sensors, 0.2D) + bias;
+		dif=Math.abs(prevSteering) - Math.abs(currentSteer);
+		if (dif > 0.4)
+			if ((prevSteering > 0 && currentSteer < 0) || (prevSteering < 0 && currentSteer > 0))
+				currentSteer = currentSteer * 0.06;
+			else
+				currentSteer = currentSteer * 0.7;
+		else if(dif>0.1)
+			if ((prevSteering > 0 && currentSteer < 0) || (prevSteering < 0 && currentSteer > 0))
+				currentSteer = currentSteer * 0.09;
+//		TODO: take in considaration the mean of the last 3-5 steerings
+		prevSteering = currentSteer;
+
+		action.steering = currentSteer;
+		System.out.println(bias.toString() + " " + dif + " " + Double.toString(action.steering));
+		if (sensors.getSpeed() > desiredSpeed) {
+			action.accelerate = 0.0D;
+			action.brake = 0.0D;
+		}
+		if (sensors.getSpeed() > desiredSpeed + 10.0D) {
+			action.accelerate = 0.0D;
+			action.brake = 0.6D;
+		}
+		if (sensors.getSpeed() <= desiredSpeed) {
+			action.accelerate = (desiredSpeed - sensors.getSpeed() / 4) / desiredSpeed;
+			action.brake = 0.0D;
+		}
+		if (sensors.getSpeed() < (desiredSpeed * 3 / 4)) {
+			action.accelerate = 1.0D;
+			action.brake = 0.0D;
+		}
+
+		inputAr.add(input);
+		speedAr.add(desiredSpeed);
+		steeringAr.add(action.steering);
+	}
 
 	public String getDriverName() {
-        return "XVII";
-    }
+		return "XVII";
+	}
 
-    public void controlQualification(Action action, SensorModel sensors) { }
+	public void controlQualification(Action action, SensorModel sensors) {
+	}
 
-    public void controlRace(Action action, SensorModel sensors) {}
+	public void controlRace(Action action, SensorModel sensors) {
+	}
 
-    public void defaultControl(Action action, SensorModel sensors){}
+	public void defaultControl(Action action, SensorModel sensors) {
+	}
 
-    @Override
-    public double getSteering(SensorModel sensorModel) {
-        return 0;
-    }
+	@Override
+	public double getSteering(SensorModel sensorModel) {
+		return 0;
+	}
 
-    @Override
-    public double getAcceleration(SensorModel sensorModel) {
-        return 0;
-    }
+	@Override
+	public double getAcceleration(SensorModel sensorModel) {
+		return 0;
+	}
 
-    public double getBraking(SensorModel sensorModel){
-        return 0;
-    }
+	public double getBraking(SensorModel sensorModel) {
+		return 0;
+	}
 
 	public ArrayList<ArrayList<Double>> getInput() {
 		// TODO Auto-generated method stub
